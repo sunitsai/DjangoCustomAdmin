dc6096067ad81c5cd450249abca6d8465e94f8f4
sunitsai <58470631+sunitsai@users.noreply.github.com>
2021-07-28 16:55:43 +0530

    First Commit

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    Firstname = models.CharField(max_length=50)
+    Lastname = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=50)
+    Contact = models.CharField(max_length=50)
+    State = models.CharField(max_length=50)
+    City = models.CharField(max_length=50)
